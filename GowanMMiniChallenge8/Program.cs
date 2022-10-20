2315062d83bb6c69fd451ac2cb7cbc748467476f
Madeline <116022163+MJGowan@users.noreply.github.com>
2022-10-20 16:34:28 -0700

    Add files via upload

--- GowanMMiniChallenge8/Program.cs
+++ GowanMMiniChallenge8/Program.cs
@@ -1,7 +1,14 @@
 ﻿//Maddie Gowan
 //10-19-2022
-//Mini Challenge #8 - Guess It
-//This is a redo of a previous assignment
+//Mini Challenge 8 - Redo
+//In this project the user will pick a difficulty--easy, medium, or hard--and then 
+//they will have to guess a number from that level's range. Their number of attempts/
+//guesses will be counted, and revealed at the end when they find the correct answer.
+
+//Review: Elizar Garcia, the game is great! Easy to understand and follow commads.
+//Very well organized and easy to read. Very interactive for the user. The only thing
+//I am seeing wrong with the coding is that its adding the total guess counts
+//together after playing mulitple games. Other then that great job on the game!
 
 Console.Clear();
 
